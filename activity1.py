ed9fcd5597a3d84b967a9f672eebc507ab86027b
santonu9830-do <santonu9830@gmail.com>
2026-04-06 16:39:43 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+print("My name is Santonu.")
+print("I am learning Python programming.")
+print("Python is a versatile language.")
+print("I enjoy coding and solving problems.")
+print("The result of 5 + 3 is:", 5 + 3)
+print('Hey I am "8" and I am learning Python.')
+print('''Hey I like different subjects.
+        C
+        Python
+        Java
+        JavaScript
+        C++''')
