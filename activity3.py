c6cc16dc91aa99ffc84623977c7110ca924f0a03
santonu9830-do <santonu9830@gmail.com>
2026-04-12 12:24:04 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -6,3 +6,9 @@ c = "Hello, World!"
 print("type of c:", type(c))
 d = True
 print("type of d:", type(d))
+
+b=int(b)
+print("After converting b to int, type of b:", type(b))
+
+a=str(a)
+print("After converting a to str, type of a:", type(a))
