633d617d3482860e8a646a550141ddc069b58496
santonu9830-do <santonu9830@gmail.com>
2026-04-18 10:14:34 +0530

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,14 @@
+# Storing birthday dates in 5 variables
+
+birthday1 = "01-08-2011"
+birthday2 = "15-03-2010"
+birthday3 = "22-11-2009"
+birthday4 = "05-06-2012"
+birthday5 = "19-12-2008"
+
+# Printing the birthdays
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
