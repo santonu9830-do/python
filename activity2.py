30288d3ac14719ab9dab3d15594f6236ce2d8399
santonu9830-do <santonu9830@gmail.com>
2026-04-19 12:27:09 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,7 +1,8 @@
-Amount =int(input("Enter the amount: "))
-note_1 = Amount//100
-note_2 = (Amount%100)//50
-note_3 = ((Amount%100)%50)//10
-print("The number of 100 notes is", note_1)
-print("The number of 50 notes is", note_2)
-print("The number of 10 notes is", note_3)
+cost_price = float(input("Enter the Cost Price: "))
+selling_price = float(input("Enter the Selling Price: "))
+if selling_price > cost_price:
+    profit = selling_price - cost_price
+    profit_percentage = (profit / cost_price) * 100
+    print("Total profit = {0}".format(profit))
+    
+    
