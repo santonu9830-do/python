7a282e6c237a564eb5b442d0d3e781196b8340dd
santonu9830-do <santonu9830@gmail.com>
2026-04-19 12:16:23 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,10 +1,7 @@
-tree1 = 98
-tree2 = 76
-tree3 = 54
-tree4 = 32
-tree5 = 10
-sum = tree1 + tree2 + tree3 + tree4 + tree5
-average = sum / 5
-print("The average height of the trees is", average, "feet.")
+num = 3
+if num > 0:
+    print(num, "is a positive number.")
 
-
+num = -1
+if num > 0:
+    print(num, "is a positive number.")
