678c6d88ad407038d23746a5a07933ce890cf618
santonu9830-do <santonu9830@gmail.com>
2026-04-25 12:25:53 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,8 +1,23 @@
-cost_price = float(input("Enter the Cost Price: "))
-selling_price = float(input("Enter the Selling Price: "))
-if selling_price > cost_price:
-    profit = selling_price - cost_price
-    profit_percentage = (profit / cost_price) * 100
-    print("Total profit = {0}".format(profit))
-    
-    
+a = 10
+b = 12
+c = 12
+
+print(not (a == b))
+print(not (b == c))
+
+a = "apple"
+b = "banana"
+
+if a != b:
+    print("a and b are different.")
+
+a = 1
+b = 3
+
+if (a == 1) != (b == 5):
+    print("Hello")
+
+a = int(input("Enter an integer: "))
+
+if not (a % 2 == 0):
+    print(a, "is not an even number. It is odd.")
