5e761a2b8fef66d81a77135701af9b8ece26d2c0
santonu9830-do <santonu9830@gmail.com>
2026-04-25 12:16:37 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,7 +1,27 @@
-num = 3
-if num > 0:
-    print(num, "is a positive number.")
 
-num = -1
-if num > 0:
-    print(num, "is a positive number.")
+a = 10
+b = 12
+c = 5
+
+
+if a and b and c:
+    print("all the e boolean numbers have true value")
+else:
+    print("at least one of the boolean numbers has false value")
+
+
+a = -5
+b = 0
+c = 8
+
+
+if a > 0 or b > 0:
+    print("either is greater than 0")
+else:
+    print("no number is greater than 0")
+
+if b > 0 or c > 0:
+    print("either is greater than 0")
+else:
+    print("no number is greater than 0")
+
