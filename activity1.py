3222a85b6bf52a089db052a72b9b70f24a461fb6
santonu9830-do <santonu9830@gmail.com>
2026-04-26 12:17:23 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,27 +1,12 @@
+medical_cause = input("Do you have a medical cause? (Y/N): ")
 
-a = 10
-b = 12
-c = 5
-
-
-if a and b and c:
-    print("all the e boolean numbers have true value")
+if medical_cause.upper() == 'Y':
+    if input("Do you have a medical certificate? (Y/N): ").upper() == 'Y':
+        print("You are allowed to give the exam.")
 else:
-    print("at least one of the boolean numbers has false value")
-
-
-a = -5
-b = 0
-c = 8
-
-
-if a > 0 or b > 0:
-    print("either is greater than 0")
-else:
-    print("no number is greater than 0")
-
-if b > 0 or c > 0:
-    print("either is greater than 0")
-else:
-    print("no number is greater than 0")
-
+    attendance = float(input("Enter your attendance percentage: "))
+    
+    if attendance > 75:
+        print("You are allowed to give the exam.")
+    else:
+        print("You are NOT allowed to give the exam.")
