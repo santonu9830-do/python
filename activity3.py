81074145d45b88d3352cd78667ec61542d634346
santonu9830-do <santonu9830@gmail.com>
2026-04-26 12:43:59 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,13 +1,28 @@
-height = float(input("Enter your height in cm: "))
-weight = float(input("Enter your weight in kg: "))
-bmi = weight / (height / 100) ** 2
-print("Your BMI is:", bmi)
+print("Select your ride:")
+print("1. Bike")
+print("2. Car")
 
-if bmi < 18.5:
-    print("You are underweight.")
-elif 18.5 <= bmi < 25:
-    print("You have a normal weight.")
-elif 25 <= bmi < 30:
-    print("You are overweight.")
-else:
-    print("You are obese.")
+choice = int(input("Enter your choice (1 or 2): "))
+if (choice == 1):
+    print("You have selected Bike.")
+    print("Select your bike type:")
+    print("1. Sports Bike")
+    print("2. Cruiser Bike")
+    bike_choice = int(input("Enter your choice (1 or 2): "))
+    if bike_choice == 1:
+        print("You have selected Sports Bike.")
+    elif bike_choice == 2:
+        print("You have selected Cruiser Bike.")
+if (choice == 2):
+    print("You have selected Car.")
+    print("Select your car type:")
+    print("1. Sedan")
+    print("2. Hatchback")
+    car_choice = int(input("Enter your choice (1 or 2): "))
+
+    if car_choice == 1:
+        print("You have selected Sedan.")
+    elif car_choice == 2:
+        print("You have selected Hatchback.")
+    else: 
+        print("Wrong choice!!.")
