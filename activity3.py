b7d408c90e25dc7be99b6723fe3eddcab1661bdb
santonu9830-do <santonu9830@gmail.com>
2026-05-03 12:28:08 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,28 +1,12 @@
-print("Select your ride:")
-print("1. Bike")
-print("2. Car")
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
 
-choice = int(input("Enter your choice (1 or 2): "))
-if (choice == 1):
-    print("You have selected Bike.")
-    print("Select your bike type:")
-    print("1. Sports Bike")
-    print("2. Cruiser Bike")
-    bike_choice = int(input("Enter your choice (1 or 2): "))
-    if bike_choice == 1:
-        print("You have selected Sports Bike.")
-    elif bike_choice == 2:
-        print("You have selected Cruiser Bike.")
-if (choice == 2):
-    print("You have selected Car.")
-    print("Select your car type:")
-    print("1. Sedan")
-    print("2. Hatchback")
-    car_choice = int(input("Enter your choice (1 or 2): "))
-
-    if car_choice == 1:
-        print("You have selected Sedan.")
-    elif car_choice == 2:
-        print("You have selected Hatchback.")
-    else: 
-        print("Wrong choice!!.")
+if sum == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num,"is not an Armsstrong number")
