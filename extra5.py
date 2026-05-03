f7ed9d354a2d26fb3ddf48df39171e54af5f87cf
santonu9830-do <santonu9830@gmail.com>
2026-05-03 12:50:02 +0530

    Add files via upload

---
+++ extra5.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+rev = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    rev = (rev * 10) + digit
+    temp = temp // 10
+print("Reversed number is", rev)
