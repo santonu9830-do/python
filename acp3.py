a50ce4787d1bc31a8bfe5555214b4f50840d8787
santonu9830-do <santonu9830@gmail.com>
2026-05-09 11:05:13 +0530

    Add files via upload

---
+++ acp3.py
@@ -0,0 +1,10 @@
+# Taking age as input from the user
+age = int(input("Enter your age: "))
+
+# Checking the condition
+if 10 <= age <= 20:
+    print("The age is between 10 and 20.")
+else:
+    print("The age is NOT between 10 and 20.")
+
+
