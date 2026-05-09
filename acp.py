dda1af5c73b45d15f1293863dfab1fd897ee30fa
santonu9830-do <santonu9830@gmail.com>
2026-05-09 10:11:34 +0530

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,8 @@
+# Python program to calculate the n-th power of a number
+
+num = int(input("Enter the number: "))
+n = int(input("Enter the power: "))
+
+result = num ** n
+
+print("Result =", result)
