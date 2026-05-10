827a9e6599895d2bbecda1cec81d11dbfc9c78be
santonu9830-do <santonu9830@gmail.com>
2026-05-10 12:34:25 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,12 +1,27 @@
-num = int(input("Enter a number: "))
-sum = 0
-temp = num
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10
-
-if sum == num:
-    print(num, "is an Armstrong number")
+rowSize = int(input("Enter the number of rows: "))
+if rowSize%2==0:
+    halfDiamRow = int(rowSize/2)
 else:
-    print(num,"is not an Armsstrong number")
+    halfDiamRow = int((rowSize+1)/2)
+space = halfDiamRow-1
+for i in range(1, halfDiamRow+1):
+    for j in range(1, space+1):
+        print(" ", end="")
+    space -= 1
+    num = 1
+    for j in range(2*i-1):
+        print(end=str(num))
+        num += 1
+    print()
+space = 1
+for i in range(halfDiamRow-1, 0, -1):
+    for j in range(1, space+1):
+        print(" ", end="")
+    space += 1
+    num = 1
+    for j in range(2*i-1):
+        print(end=str(num))
+        num += 1
+    print()
+
+
