376ed33891668a0ce35820a604fab1a26c3bad81
santonu9830-do <santonu9830@gmail.com>
2026-05-16 12:27:04 +0530

    Add files via upload

--- act3.py
+++ act3.py
@@ -1,21 +1,13 @@
-num = int(input("Please enter a number : "))
-t = num
-numLen = 0
-while t > 0:
-    numLen += 1
-    t = int(t / 10)
-if numLen >=4:
- numLen = int(numLen / 2)
- chk = 0
- while num > 0:
-    rem = num % 10
-    if chk==numLen:
-       midone = rem
-    elif chk==(numLen-1):
-       midtwo = rem
-    num = int(num / 10)
-    chk += 1
-    prod = midone * midtwo
- print("/nProduct of Mid digits (" + str(midone) + " and " + str(midtwo) + ") is : ", prod)
-else:
-    print("/nIt's not a four-digit number or more than 4 digits number!.")
+import turtle
+my_wn = turtle.Screen()
+my_wn.bgcolor("lightgreen")
+my_wn.title("Turtle")
+my_turtle = turtle.Turtle()
+size = 0
+while True:
+    for i in range(4):
+        my_turtle.forward(size + 1)
+        my_turtle.left(90)
+    size += 1
+    size -= 5       
+turtle.done()
