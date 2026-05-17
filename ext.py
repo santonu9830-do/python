babe87abf8ee7bfbc2e1f188a84541bebdc35ab0
santonu9830-do <santonu9830@gmail.com>
2026-05-17 12:28:49 +0530

    Add files via upload

--- ext.py
+++ ext.py
@@ -1,7 +1,11 @@
-rows = int(input("Enter the total number of rows: "))
-
-for i in range(rows, 0, -1):
-    spaces = rows - i
-    stars = 2 * i - 1
-
-    print(" " * spaces + "*" * stars)
+def perimeter():
+    # Calculate the perimeter of a square 
+    sides = int(input("Enter the number of sides of the square: "))
+    perimeter = 4 * (sides)
+    print("The perimeter of the square is:", perimeter)
+    # Calculate the perimeter of a rectangle
+    length = int(input("Enter the length of the rectangle: "))
+    sides = int(input("Enter the width of the rectangle: "))
+    perimeter = 2 * (length + sides)
+    print("The perimeter of the rectangle is:", perimeter)
+perimeter()
