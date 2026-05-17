656e42a893773f4f9cb7963b1350bee2f69c1f8c
santonu9830-do <santonu9830@gmail.com>
2026-05-17 12:47:31 +0530

    Add files via upload

--- ext2.py
+++ ext2.py
@@ -1,7 +1,18 @@
 def convert_number(decimal_number):
-    print("Binary number:", bin(decimal_number))
-    print("Octal number:", oct(decimal_number))
-    print("Hexadecimal number:", hex(decimal_number))
+    
+    print("select optionn for binary, o for octal and h for hexadecimal")
+    print("b. Binary")
+    print("o. Octal")
+    print("h. Hexadecimal")
+    choice = input("Enter choice (b/o/h): ")
+    if choice == 'b':
+        print("Binary number:", bin(decimal_number))
+    elif choice == 'o':
+        print("Octal number:", oct(decimal_number))
+    elif choice == 'h':
+        print("Hexadecimal number:", hex(decimal_number))
+    else:
+        print("Invalid input")
 
-number = int(input("Enter a decimal number: "))
-convert_number(number)
+decimal_number = int(input("Enter a decimal number: "))
+convert_number(decimal_number)
