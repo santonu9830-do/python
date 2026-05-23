ec8b1084e145d681825ed7ce25e34ea2892c67a6
santonu9830-do <santonu9830@gmail.com>
2026-05-23 12:44:40 +0530

    Add files via upload

--- act3.py
+++ act3.py
@@ -1,26 +1,11 @@
-def add(P, Q):
-    return P + Q
-def subtract(P, Q):
-    return P - Q
-def multiply(P, Q):
-    return P * Q
-def divide(P, Q):
-    return P / Q
-print("Please select an operation:")
-print("a. Add")
-print("b. Subtract")
-print("c. Multiply")
-print("d. Divide")
-choice = input("Enter choice (a/b/c/d): ")
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-if choice == 'a':
-    print(num1, "+", num2, "=", add(num1, num2))
-elif choice == 'b':
-    print(num1, "-", num2, "=", subtract(num1, num2))
-elif choice == 'c':
-    print(num1, "*", num2, "=", multiply(num1, num2))
-elif choice == 'd':
-    print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("this is an Invalid input")
+def factorial(num):
+    if num==0:
+        return 1
+    else:
+        return num*factorial(num-1)
+print(factorial.__doc__)
+print("the factorial of 5 is:",factorial(5))
+print("the factorial of 0 is:",factorial(0))
+print("the factorial of 10 is:",factorial(10))
+print("the factorial of 20 is:",factorial(20))
+print("the factorial of 100 is:",factorial(100))
