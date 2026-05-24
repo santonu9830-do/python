19efdb5a8539cb17de5416bcf39f0c9e227c8390
santonu9830-do <santonu9830@gmail.com>
2026-05-24 12:37:16 +0530

    Add files via upload

--- act3.py
+++ act3.py
@@ -1,11 +1,8 @@
-def factorial(num):
-    if num==0:
-        return 1
-    else:
-        return num*factorial(num-1)
-print(factorial.__doc__)
-print("the factorial of 5 is:",factorial(5))
-print("the factorial of 0 is:",factorial(0))
-print("the factorial of 10 is:",factorial(10))
-print("the factorial of 20 is:",factorial(20))
-print("the factorial of 100 is:",factorial(100))
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+
+    print('\nCurrent variable value:', var)
+print('\nGood bye!')
