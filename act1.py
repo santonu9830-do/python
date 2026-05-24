ae0784430091c46e91c386a1629434024912a27a
santonu9830-do <santonu9830@gmail.com>
2026-05-24 12:25:00 +0530

    Add files via upload

--- act1.py
+++ act1.py
@@ -1,5 +1,13 @@
-def total_calc(bill_amount,tip_perc):
-    total = bill_amount*(1+0.01*tip_perc)
-    total = round(total,2)
-    print(f"please pay: {total}")
-total_calc(100,15)
+a = input("Enter a word: ")
+b = input("Enter a letter: ")
+
+for i in a:
+    if i == b:
+
+        print("The letter b found in the word")
+        break
+    
+    else:
+        print("The letter b not found in the word")
+
+    
