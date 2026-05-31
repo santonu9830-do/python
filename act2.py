6342f6e71959658a599d94c4e84a35f82129cc42
santonu9830-do <santonu9830@gmail.com>
2026-05-31 12:28:12 +0530

    Add files via upload

--- act2.py
+++ act2.py
@@ -1,16 +1,30 @@
-for x in range(10):
-    
-   if x % 20 == 0:
-     print("twist")
+import random
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
-   elif x % 15 == 0:
-    pass
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
 
-   elif x % 3 == 0:
-    print("buzz")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
 
-   elif x % 5 == 0:
-    print("rizz")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
 
-   else:
-    print(x)
+            play_again = input("Play again? (y/n): ")
+            if play_again != "y":
+                break
