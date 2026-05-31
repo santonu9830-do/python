e4d54e31f32447b1eb14d97207891ad405d74caa
santonu9830-do <santonu9830@gmail.com>
2026-05-31 12:37:56 +0530

    Add files via upload

--- act3.py
+++ act3.py
@@ -1,8 +1,11 @@
-var = 10
-while var > 0:
-    var = var - 1
-    if var == 5:
-        continue
+import math
+print('The floor and ceiling of 24.5 are:')
+print(str(math.floor(24.5)) + ' , ' + str(math.ceil(24.5)))
 
-    print('\nCurrent variable value:', var)
-print('\nGood bye!')
+x = 10
+y = -15
+
+print('The value of x after copying the sign from y is: ' + str(math.copysign(x, y)))
+print('Absolute value of -96 and 56 are respectively: ' + str(math.fabs(-96)) + ' , ' + str(math.fabs(56)))
+
+print('The GCD of 24 and 56 : ' + str(math.gcd(24, 56)))
