63963abcbb53135a0fd73b215e9ff9738b0e9504
santonu9830-do <santonu9830@gmail.com>
2026-06-07 12:38:02 +0530

    Add files via upload

---
+++ AverageAndsum.py
@@ -0,0 +1,13 @@
+L = [4, 5, 6, 7, 8]
+print ("Original List: ", L)
+
+avg = sum(L) / len(L)
+print ("Average of the List: ", avg)
+print ("Sum of the List: ", sum(L))
+
+largest = max(L)
+print ("Largest element in the List: ", largest)
+smallest = min(L)
+print ("Smallest element in the List: ", smallest)
+
+
