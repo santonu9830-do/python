3684a73aec278638a8c8162cba161a292196490e
santonu9830-do <santonu9830@gmail.com>
2026-06-07 12:20:04 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,13 @@
+empty_list = []
+print()
+
+# List of integers
+integers = [1, 2, 3, 4, 5]
+print(integers)
+
+triples = [1, 3, 5,] * 3
+print(triples)
+
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList,"\n")
