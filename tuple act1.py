0196a62d6dbe4fcaf20d4a6ad0db40f07305eb52
santonu9830-do <santonu9830@gmail.com>
2026-06-13 12:17:28 +0530

    Add files via upload

---
+++ tuple act1.py
@@ -0,0 +1,17 @@
+tuplex = ("tuple", False, 3.14, 42, )
+print(tuplex)
+tuplex = (4, 6, 2, 8, 10)
+print(tuplex)
+
+tuplex = tuplex +(9,)
+print(tuplex)
+
+tuple1 = (50, 10, 30, 40, 50)
+print(tuple1)
+
+tuple1 = (50, 10, 30, 40, 50)
+print(tuple1.count(50))
+
+tuplex = (1, 2, 3, 4, 5)
+_slice = tuplex[3:5] 
+print(_slice)
