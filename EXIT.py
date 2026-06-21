a20937ab7d2fecd7979b8d343602f7605ad9a9f9
santonu9830-do <santonu9830@gmail.com>
2026-06-21 12:43:40 +0530

    Add files via upload

---
+++ EXIT.py
@@ -0,0 +1,7 @@
+my_list = [1,2,3,4,5,5,9,8,7,2,6,4,3,2,1]
+
+for i in my_list:
+    if i%2 == 0:
+        print("even number found and the value is :", i)
+        print("exiting the program. . .")
+        exit()
