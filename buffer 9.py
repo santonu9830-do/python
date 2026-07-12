2f915d5dc731ee163a372bbb0243f9294e4223bf
santonu9830-do <santonu9830@gmail.com>
2026-07-12 12:52:26 +0530

    Add files via upload

---
+++ buffer 9.py
@@ -0,0 +1,29 @@
+﻿from abc import ABC, abstractmethod
+
+
+class Employee(ABC):
+    def display(self, name):
+        print(f"Employee Name: {name}")
+
+    @abstractmethod
+    def work(self):
+        pass
+
+
+class Developer(Employee):
+    def work(self):
+        print("Developer is writing code.")
+
+
+class Teacher(Employee):
+    def work(self):
+        print("Teacher is teaching students.")
+
+
+dev = Developer()
+dev.display("Alice")
+dev.work()
+
+teach = Teacher()
+teach.display("Bob")
+teach.work()
